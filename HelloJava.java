f96cc3fd6d0544df6608a295df451b695ac3be40
Kim yong hyeon <90203932+Cocomong98@users.noreply.github.com>
2024-12-08 23:04:20 +0900

    Java

---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+// Hello.java를 클래스라고 함. 파일명과 클래스명은 같아야 함
+public class HelloJava {
+
+    // 여기가 프로그램을 실행하는 시작지점이다
+    public static void main(String args[]){
+        // print는 값을 console에 출력하는 기능
+        System.out.println("Hello Java!");
+    } // main 메서드의 범위 끝
+
+} // HelloJava 메서드의 범위 끝
---
+++ HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+    public static void main(String args[]){
+        // 실행은 main 줄을 시작으로 내림차순으로 진행
+        System.out.println("Hello Java1");
+        System.out.println("Hello Java2");
+        System.out.println("Hello Java3");
+    }
+}
