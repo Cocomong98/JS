c62fff6e59a88b5db468830feda86de0425c648d
Kim yong hyeon <90203932+Cocomong98@users.noreply.github.com>
2024-12-10 23:49:34 +0900

    swap

---
+++ 12912.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    var answer = 0;
+    if (a > b) {
+        [a, b] = [b, a];
+    }
+
+    for (var i = a; i <= b; i++) {
+        answer += i;
+    }
+    return answer;
+}
