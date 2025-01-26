ed4f602c746a2981a1a1bf9f4ae20c481b833485
Cocomong98 <yonghjjm0618@gmail.com>
2025-01-26 23:44:56 +0900

    250126

--- Lv1/12915.js
+++ Lv1/12915.js
@@ -8,6 +8,7 @@ function solution(strings, n) {
     }
     // 추출한 글자 정렬
     index = index.sort();
+    // 원본 배열도 정렬함 (출력시에 추출글자별 정렬 + 같은 문자에서는 앞글자 정렬이 되어야 하기 때문)
     strings = strings.sort();
     // 원본 배열에 재배열된 인덱스 배열의 인덱스를 갖도록 재배치
     for (var i = 0; i < index.length; i++) {
