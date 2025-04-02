2027300da169631bc065be30d9ec0758aa681423
Cocomong98 <yonghjjm0618@gmail.com>
2025-04-02 23:23:15 +0900

    250402

--- Lv1/70128.js
+++ Lv1/70128.js
@@ -1,7 +1,10 @@
 function solution(a, b) {
     var answer = 0;
+    // 받은 문자열의 길이만큼 반복
     for (var i = 0; i < a.length; i++) {
+        // 곱셈 수행
         answer += a[i] * b[i];
     }
+    // 결과값 출력
     return answer;
 }
